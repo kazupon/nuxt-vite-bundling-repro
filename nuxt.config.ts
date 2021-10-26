2953e8c2904062b033b1464935eaafd42f51ffd6
kazuya kawaguchi <kawakazu80@gmail.com>
2021-10-26 19:05:08 +0900

    commit first

---
+++ nuxt.config.ts
@@ -0,0 +1,19 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+export default defineNuxtConfig({
+  buildModules: ['@intlify/nuxt3'],
+  // vite: false,
+  intlify: {
+    vueI18n: {
+      locale: 'en',
+      messages: {
+        en: {
+          hello: 'Hello'
+        },
+        ja: {
+          hello: 'こんにちは'
+        }
+      }
+    }
+  }
+})
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <h1>Hello Nuxt3!</h1>
+    <p>{{ t('hello') }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
